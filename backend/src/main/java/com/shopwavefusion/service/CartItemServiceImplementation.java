8bb8bb8e50db66a326511ac778374b2b40383bd5
sebas123312231 <arceantezanasebastianalejandro@gmail.com>
2026-06-12 01:45:23 -0400

    fix(cart): respect requested quantity when creating cart item
    Removed hardcoded setQuantity(1) in createCartItem that was overriding
    the quantity passed from the frontend, causing all items to be added
    with quantity 1 regardless of user selection.

--- backend/src/main/java/com/shopwavefusion/service/CartItemServiceImplementation.java
+++ backend/src/main/java/com/shopwavefusion/service/CartItemServiceImplementation.java
@@ -28,7 +28,6 @@ public class CartItemServiceImplementation implements CartItemService {
 	@Override
 	public CartItem createCartItem(CartItem cartItem) {
 		
-		cartItem.setQuantity(1);
 		cartItem.setPrice(cartItem.getProduct().getPrice()*cartItem.getQuantity());
 		cartItem.setDiscountedPrice(cartItem.getProduct().getDiscountedPrice()*cartItem.getQuantity());
 		
